0d8cab94dd6fe88f54b1d7ac3df71af578440df6
LuisMendez <lmiguel05c@gmail.com>
2025-09-24 12:06:47 -0500

    estructura de carpetas

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My PHP Page</title> 
+</head>
+<body>
+    <h1>Welcome to My PHP Page</h1>
+    <?php
+        echo "<p>This is a simple PHP script embedded in HTML.</p>";
+    ?>
+</body>
+</html>
